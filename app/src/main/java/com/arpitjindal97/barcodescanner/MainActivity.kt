af9716561a61bd2df981cd775cfe6b88adc9c16b
Arpit Agarwal <arpitjindal97@gmail.com>
2018-06-08 20:50:14 +0530

    send to server dialog added

--- app/src/main/java/com/arpitjindal97/barcodescanner/MainActivity.kt
+++ app/src/main/java/com/arpitjindal97/barcodescanner/MainActivity.kt
@@ -5,16 +5,24 @@ import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.google.zxing.Result
 import kotlinx.android.synthetic.main.activity_main.*
 import me.dm7.barcodescanner.zxing.ZXingScannerView
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
 
 
 class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
@@ -22,8 +30,7 @@ class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
     private var TAG: String = "arpit"
 
     private var flash: Boolean = false
-
-    private lateinit var db: DatabaseHelper
+    var scan_result: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,16 +76,23 @@ class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
     override fun handleResult(rawResult: Result?) {
         // Do something with the result here
+
+        scan_result = rawResult?.text
         Log.v(TAG, rawResult?.text)
         Log.v(TAG, rawResult?.barcodeFormat.toString())
 
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setTitle("Scan Result")
-        builder.setMessage(rawResult?.text)
+        builder.setMessage(scan_result)
         builder.setPositiveButton("Send", sendButtonListener)
-        var alert1: AlertDialog = builder?.create()
+
+        var alert1 = builder?.create()
+
         alert1.show()
 
+    }
+
+    fun ResumeCameraPreview() {
         scanner_view?.resumeCameraPreview(this)
     }
 
@@ -101,8 +115,80 @@ class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
         }
     }
 
+
     private var sendButtonListener = DialogInterface.OnClickListener { dialog: DialogInterface?, which: Int ->
         Log.v("arpit", "button clicked")
+
+        lateinit var progressdialog: AlertDialog
+
+        class MyAsyncTask() : AsyncTask<Void, Void, String>() {
+
+            override fun doInBackground(vararg params: Void?): String {
+
+                return if (ServerStatus() == "Running") {
+                    SendResult(scan_result)
+                } else {
+                    "Error"
+                }
+            }
+
+            override fun onPreExecute() {
+                super.onPreExecute()
+                dialog?.dismiss()
+                var progressdialog_view = layoutInflater.inflate(R.layout.progressbar_dialog, null)
+                var builder = AlertDialog.Builder(this@MainActivity)
+                builder.setView(progressdialog_view)
+                progressdialog = builder?.create()
+                progressdialog.show()
+            }
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.v(TAG, result)
+
+                progressdialog.findViewById<TextView>(R.id.progress_title).text = "Sent"
+                progressdialog.findViewById<ProgressBar>(R.id.indeterminateBar).isIndeterminate = false
+                progressdialog.findViewById<ProgressBar>(R.id.indeterminateBar).progress = 100
+                progressdialog.findViewById<Button>(R.id.progress_dialog_button).isEnabled = true
+                progressdialog.findViewById<Button>(R.id.progress_dialog_button).setOnClickListener {
+                    progressdialog.dismiss()
+                    ResumeCameraPreview()
+                }
+
+            }
+
+
+        }
+
+        var task = MyAsyncTask().execute()
+
+    }
+
+    fun ServerStatus(): String {
+        var url = DatabaseHelper(baseContext).Get().StatusURL
+
+        var request = Request.Builder()
+                .url(url)
+                .build()
+
+        var client = OkHttpClient()
+        var resp = client.newCall(request).execute()
+        return resp.body()?.string().toString()
+
+    }
+
+    fun SendResult(result: String?): String {
+
+        var url = DatabaseHelper(this).Get().ResultURL
+
+        var dataType: MediaType? = MediaType.parse("text/plain;charset=utf-8")
+
+        var data = RequestBody.create(dataType, result)
+        var request = Request.Builder().url(url).post(data).build()
+
+        var resp = OkHttpClient().newCall(request).execute()
+
+        return resp.body()?.string().toString()
 
     }
 
--- app/src/main/java/com/arpitjindal97/barcodescanner/Misc.kt
+++ app/src/main/java/com/arpitjindal97/barcodescanner/Misc.kt
@@ -4,9 +4,19 @@ class Server {
 
     constructor() {}
     constructor(ip: String, port: String) {
-        ip_address  = ip
+        ip_address = ip
         port_number = port
     }
-     lateinit  var ip_address: String
-    lateinit  var port_number: String
+
+    lateinit var ip_address: String
+    lateinit var port_number: String
+
+    var url: String = "temp"
+        get() = "$ip_address:$port_number"
+
+    var ResultURL = ""
+        get() = "http://$url/Result"
+
+    var StatusURL = ""
+        get() = "http://$url/Status"
 }
