98ee66c22947bf13b859ff2223b25228009ebde9
Arpit Agarwal <arpitjindal97@gmail.com>
2018-06-16 04:09:48 +0530

    added open source licenses

--- app/src/main/java/com/arpitjindal97/barcodescanner/SettingsFragment.kt
+++ app/src/main/java/com/arpitjindal97/barcodescanner/SettingsFragment.kt
@@ -1,10 +1,14 @@
 package com.arpitjindal97.barcodescanner
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.preference.Preference
 import android.preference.PreferenceFragment
 import android.preference.PreferenceManager
 import android.view.MenuItem
+import com.mikepenz.aboutlibraries.Libs
+import com.mikepenz.aboutlibraries.LibsBuilder
 
 class SettingsFragment : PreferenceFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +18,9 @@ class SettingsFragment : PreferenceFragment() {
         bindPreferenceSummaryToValue(findPreference("ip_address"))
         bindPreferenceSummaryToValue(findPreference("port_number"))
         bindPreferenceSummaryToValue(findPreference("scan_type"))
+
+        bindPreferenceOnClickListener(findPreference("open_source_lib"))
+        bindPreferenceOnClickListener(findPreference("author"))
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -49,6 +56,25 @@ class SettingsFragment : PreferenceFragment() {
                             .getString(preference.key, ""))
         }
 
+        private fun bindPreferenceOnClickListener(preference: Preference) {
+            preference.setOnPreferenceClickListener { preference ->
 
+                if (preference.key == "open_source_lib") {
+                    LibsBuilder()
+                            .withActivityStyle(Libs.ActivityStyle.LIGHT_DARK_TOOLBAR)
+                            .withActivityTitle("Open source libraries")
+                            .withAboutIconShown(true)
+                            .withAboutVersionShown(true)
+                            .withAboutDescription(preference.context.resources.getString(R.string.app_description))
+                            .start(preference.context)
+                } else if (preference.key == "author") {
+
+                    val browserIntent = Intent(Intent.ACTION_VIEW,
+                            Uri.parse(preference.context.resources.getString(R.string.author_url)))
+                    preference.context.startActivity(browserIntent)
+                }
+                true
+            }
+        }
     }
 }
